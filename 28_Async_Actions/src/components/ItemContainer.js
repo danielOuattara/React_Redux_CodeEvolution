8e45bff0c31de26f528206e8dc3e0b22fc2623dd
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-02 23:23:35 +0200

    tiny code optimization

--- 28_Async_Actions/src/components/ItemContainer.js
+++ 28_Async_Actions/src/components/ItemContainer.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { buyCake } from './../redux/cakes/cakeAction';
-import { buyIceCream } from './../redux/iceCream/iceCreamAction'
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "./../redux/cakes/cakeAction";
+import { buyIceCream } from "./../redux/iceCream/iceCreamAction";
 
-// this component display the number of both cakes and ice cream  in the store
+// this component display the number of both cakes and ice cream in the store
 
 function ItemContainer(props) {
-    return (
-        <div>
-            <h2>Items : {props.item} </h2>
-            <button onClick={props.buyItem}>Buy Some Item</button>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>Items : {props.item} </h2>
+      <button onClick={props.buyItem}>Buy Some Item</button>
+    </div>
+  );
 }
 
-const mapStateToProps = (state, ownProps ) => {
-    const itemState = ownProps.cake ? 
-        state.cake.numberOfCakes : state.iceCream.numberOfIceCreams;
-    return {
-        item : itemState
-    };
-}
+const mapStateToProps = (state, ownProps) => {
+  const itemState = ownProps.cake
+    ? state.cake.numberOfCakes
+    : state.iceCream.numberOfIceCreams;
+  return {
+    item: itemState,
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    const dispatchFunction = ownProps.cake ?
-        () => dispatch(buyCake()) : () => dispatch(buyIceCream());
-    return {
-        buyItem: dispatchFunction
-    };
-} 
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps) (ItemContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
